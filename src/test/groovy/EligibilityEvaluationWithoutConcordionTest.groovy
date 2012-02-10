80d4ec87702cf38f77d18d5a24439160e7864451
sharamas <suresh.sharma@orbitbenefits.com>
2012-02-10 15:00:01 +0000

    Sucessfully configured geb

--- src/test/groovy/EligibilityEvaluationWithoutConcordionTest.groovy
+++ src/test/groovy/EligibilityEvaluationWithoutConcordionTest.groovy
@@ -1,5 +1,16 @@
 import geb.Browser
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.By
 
-Browser.drive{
-    go "http://google.co.uk"
-}
+browser = new Browser()
+browser.setDriver(new FirefoxDriver())
+browser.go("http://localhost:8080")
+browser.$("input", id: "username") << "orbitadmin"
+browser.$("input", id: "password") << "testdata"
+browser.driver.findElement(By.id('loginsubmit')).submit()
+//browser.$('#loginsubmit1').submit()
+//WebDriver driver = browser.getDriver()
+//driver.findElement(By.id("loginsubmit"))
+//assert element != null
+browser.close()
--- src/test/java/EligibilityEvaluationWithGebGroovyAcceptanceTest.groovy
+++ src/test/java/EligibilityEvaluationWithGebGroovyAcceptanceTest.groovy
@@ -1,16 +1,36 @@
 import geb.Browser
-import org.concordion.api.ResultSummary
-import org.concordion.internal.ConcordionBuilder
-import org.junit.Test
-import org.junit.runner.RunWith
 import org.concordion.integration.junit4.ConcordionRunner
+import org.junit.After
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.openqa.selenium.By
+import org.openqa.selenium.firefox.FirefoxDriver
 
 @RunWith(ConcordionRunner.class)
 class EligibilityEvaluationWithGebGroovyAcceptanceTest {
+    Browser browser = new Browser();
+    String url = "http://localhost:8080"
+    @Before
+    public void before() {
+        login()
+    }
 
     public void isEmployeeEligible(Integer age, Double basicMonthlySalary, String niNumber) {
-        Browser.drive{
-            go "http://google.co.uk"
-        }
+        browser.go(url+"/company/home.html?id=158") //Go to the company home page
+        browser.go(url + "/list/employee.html") //Go to the employee list page
+    }
+
+    private void login() {
+        browser = new Browser()
+        browser.setDriver(new FirefoxDriver())
+        browser.go("http://localhost:8080")
+        browser.$("input", id: "username") << "orbitadmin"
+        browser.$("input", id: "password") << "testdata"
+        browser.driver.findElement(By.id('loginsubmit')).submit()
+    }
+
+    @After
+    public void tear() {
+        browser.close()
     }
 }
