f299a0ad360b6bba9b6a0d147343acadc4a6fbb0
sharamas <suresh.sharma@orbitbenefits.com>
2012-02-13 12:02:15 +0000

    Reach to the point where we are able to click on radio button

--- src/test/groovy/EligibilityEvaluationWithoutConcordionTest.groovy
+++ src/test/groovy/EligibilityEvaluationWithoutConcordionTest.groovy
@@ -1,16 +1,9 @@
 import geb.Browser
 import org.openqa.selenium.firefox.FirefoxDriver
-import org.openqa.selenium.WebDriver
-import org.openqa.selenium.By
 
-browser = new Browser()
-browser.setDriver(new FirefoxDriver())
-browser.go("http://localhost:8080")
-browser.$("input", id: "username") << "orbitadmin"
-browser.$("input", id: "password") << "testdata"
-browser.driver.findElement(By.id('loginsubmit')).submit()
-//browser.$('#loginsubmit1').submit()
-//WebDriver driver = browser.getDriver()
-//driver.findElement(By.id("loginsubmit"))
-//assert element != null
-browser.close()
+Browser.drive(baseUrl: "http://localhost:8080", driver: new FirefoxDriver()) {
+    go()
+    $("input", id: "username") << "orbitadmin"
+    $("input", id: "password") << "testdata"
+    $("#loginsubmit").submit()
+}
--- src/test/java/EligibilityEvaluationWithGebGroovyAcceptanceTest.groovy
+++ src/test/java/EligibilityEvaluationWithGebGroovyAcceptanceTest.groovy
@@ -4,26 +4,57 @@ import org.junit.After
 import org.junit.Before
 import org.junit.runner.RunWith
 import org.openqa.selenium.By
-import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.chrome.ChromeDriver
 
 @RunWith(ConcordionRunner.class)
 class EligibilityEvaluationWithGebGroovyAcceptanceTest {
     Browser browser = new Browser();
     String url = "http://localhost:8080"
+
     @Before
     public void before() {
         login()
     }
 
-    public void isEmployeeEligible(Integer age, Double basicMonthlySalary, String niNumber) {
-        browser.go(url+"/company/home.html?id=158") //Go to the company home page
-        browser.go(url + "/list/employee.html") //Go to the employee list page
+    public String isEmployeeEligible(String expectedFirstName, String expectedSurname, Integer age, Double basicMonthlySalary) {
+        browser.go("/company/home.html?id=158")
+        browser.$("#employeeSection a", title: "List Employees").click()
+
+
+        def tableRows = browser.$("table tbody tr")
+        tableRows.find { tr ->
+
+            def radioButton = tr.find("td")[0]
+            println radioButton
+
+            def surname = tr.find("td")[1].text()
+            println "expectedSurname:$expectedSurname surname: $surname"
+
+            def firstName = tr.find("td")[2].text()
+            println "expectedFirstName:$expectedFirstName firstName: $firstName"
+
+            def dob = tr.find("td")[3].text()
+            println "dob: $dob"
+
+            if (surname.trim() == expectedSurname.trim()
+                && firstName.trim() == expectedFirstName.trim()
+            ) {
+                radioButton.click()
+                //TODO: Should exit closure once the radio button is clicked. But How?
+            }
+
+
+        }
+
+
+        return "Eligible"
     }
 
     private void login() {
         browser = new Browser()
-        browser.setDriver(new FirefoxDriver())
-        browser.go("http://localhost:8080")
+        browser.setDriver(new ChromeDriver())
+        browser.setBaseUrl("http://localhost:8080")
+        browser.go()
         browser.$("input", id: "username") << "orbitadmin"
         browser.$("input", id: "password") << "testdata"
         browser.driver.findElement(By.id('loginsubmit')).submit()
